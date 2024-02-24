3647c6ed09b07cf1a68f8f535b52dd1adc628b97
satumaimo77 <lucky.no0077@gmail.com>
2024-02-24 21:16:53 +0900

    questionモデル、マイグレーションファイルの作成

--- db/migrate/20240224120706_create_questions.rb
+++ db/migrate/20240224120706_create_questions.rb
@@ -1,7 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration[7.0]
   def change
     create_table :questions do |t|
-
+      t.string :title ,null: false
+      t.text :content ,null: false
+      t.string :contributor ,null: false
       t.timestamps
     end
   end
